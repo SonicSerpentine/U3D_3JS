c5eeb99ca3f901eba457be0fa8f3a2c189ef65c5
SonicSerpentine <alexshorey@gmail.com>
2016-12-30 17:32:43 -0600

    First Commit
    Creating a DVC for this threejs development pipeline exploration project

---
+++ scripts/U3D_3JS.js
@@ -0,0 +1,55 @@
+﻿/// <reference path="three.js" />
+/// <reference path="ZUTIL.js" />
+
+document.addEventListener("DOMContentLoaded", function () { new App(); }, false);
+
+App = function () {
+    var canvas = document.getElementById("threeCanvas");
+    var scene = new ZUTIL.Scene();
+    test_scene_setup_01(scene);
+
+    var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    function mainUpdate() {
+        window.requestAnimationFrame(mainUpdate);
+
+        scene.update();
+
+        renderer.render(scene, scene.activeCamera);
+    }
+    mainUpdate();
+
+    window.addEventListener("resize", onWindowResize, false);
+    function onWindowResize() {
+        scene.activeCamera.aspect = window.innerWidth / window.innerHeight;
+        scene.activeCamera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+}
+
+test_scene_setup_01 = function(scene){
+    var cube = new ZUTIL.SpinningCube(scene);
+    cube.position.z = 3
+    var cube2 = new ZUTIL.SpinningCube(scene);
+    cube2.position.x = 3;
+
+    var light = new THREE.PointLight(0xff0000, 1, 100);
+    light.position.set(0, 2, 0);
+    //scene.add(light);
+    
+    var loader = new THREE.ObjectLoader();
+    loader.setTexturePath("");
+    loader.load("./assets/test_scene_01.json", function (obj) {
+
+        //obj.children[0].material = obj.children[1].material;
+        //obj.children[0].material.emissiveIntensity = 1;
+        obj.children[0].material.lights = false;
+
+        scene.add(obj);
+    });
+    
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+
---
+++ scripts/ZUTIL.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="three.js" />
+
+class ZUTIL { }
+
+ZUTIL.Scene = class extends THREE.Scene {
+    constructor() {
+        super();
+        this.update_arr = [];
+        this.activeCamera = new ZUTIL.Camera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.activeCamera.position.y = 8;
+        this.activeCamera.position.x = -15;
+        this.activeCamera.position.z = -15;
+        this.activeCamera.lookAt(new THREE.Vector3(0, 0, 0));
+    }
+
+    update() {
+        ///<summary> runs the update callbacks of ZUTIL objects attached to this scene</summary> 
+        this.update_arr.forEach(function (e) { e(); });
+    }
+}
+
+ZUTIL.Camera = class extends THREE.PerspectiveCamera {
+    constructor(fov, aspect, near, far) {
+        super(fov, aspect, near, far);
+    }
+}
+
+ZUTIL.Vec3 = class extends THREE.Vector3{
+    constructor(x, y, z){
+        super(x, y, z);
+    }
+};
+
+ZUTIL.SpinningCube = class extends THREE.Mesh {
+    constructor(scene) {
+        ///<param name="scene" type="ZUTIL.Scene">scene the cube is attaching to</param>
+        var boxGeo = new THREE.BoxGeometry(1,1,1);
+        var boxMat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        super(boxGeo, boxMat);
+        var _this = this;
+        this.scene = scene;
+        this.scene.add(this);
+        this.callback = function(){_this.think()}
+        this.scene.update_arr.push(this.callback);
+    }
+
+    think() {
+        this.rotation.y += 0.01;
+    }
+
+    stop() {
+        for (var i = 0; i < this.scene.update_arr.length; i++){
+            if (this.scene.update_arr[i] == this.callback) {
+                this.scene.update_arr.splice(i, 1);
+            }
+        }
+    }
+}
